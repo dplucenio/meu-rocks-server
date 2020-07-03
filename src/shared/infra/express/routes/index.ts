ec3746bfd4b84bac3ec4247c8e3fff66869d0201
Daniel Plucenio <dplucenio@gmail.com>
2020-07-03 00:53:24 -0300

    rename server and http module

--- src/shared/infra/express/routes/index.ts
+++ src/shared/infra/express/routes/index.ts
@@ -1,6 +1,6 @@
 import {Router} from 'express';
-import studentsRouter from '@modules/users/infra/http/routes/students.routes';
-import sessionsRouter from '@modules/users/infra/http/routes/sessions.routes';
+import studentsRouter from '@modules/users/infra/http/express/students.routes';
+import sessionsRouter from '@modules/users/infra/http/express/sessions.routes';
 
 const router: Router = Router();
 router.use('/students', studentsRouter);
