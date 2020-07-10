1f4994a93a7e5de9a9ddb5c6e264e9217bf9646b
Daniel Plucenio <dplucenio@gmail.com>
2020-07-10 09:23:56 -0300

    intermediate work

--- src/modules/users/services/CreateUser.spec.ts
+++ src/modules/users/services/CreateUser.spec.ts
@@ -1,6 +1,8 @@
 import CreateUser from "./CreateUser";
 import FakeUserRepository from "../repositories/fakes/FakeUserRepository";
 import { parseISO, isEqual } from 'date-fns';
+import AppError from "@shared/errors/AppError";
+import { Any } from "typeorm";
 
 describe('CreateUser', () => {
   it('should be possible to create a new user', async () => {
@@ -20,13 +22,15 @@ describe('CreateUser', () => {
     let dateString = '1990-12-12';
     {
       let duque = await new CreateUser(userRepository).execute({
-        name: 'Duque',
-        nickname: 'Ducks',
+        name: 'John Doe',
+        nickname: 'Doe',
         password: '123456',
-        email: 'duque@mail.com',
+        email: 'jdoe@mail.com',
         birthday: parseISO(dateString)
       });
-      expect(duque.name).toEqual('Duque');
+      expect(duque.name).toEqual('John Doe');
+      expect(duque.email).toEqual('jdoe@mail.com');
+      expect(duque.nickname).toEqual('Doe');
       expect(isEqual(duque.birthday, parseISO('1990-12-12'))).toBe(true);
     }
   });
@@ -40,10 +44,29 @@ describe('CreateUser', () => {
         email: 'jdoe@mail.com',
         birthday: parseISO('1990-12-12')
       });
-      expect(user.name).toEqual('John Doe');
       expect(user.nickname).toEqual('John');
-      expect(isEqual(user.birthday, parseISO('1990-12-12'))).toBe(true);
     }
   });
+
+  it('should not be possible to create a user with null name', async () => {
+    const userRepository = new FakeUserRepository();
+    let name: any;
+    expect(async () => {
+      const createUserService = new CreateUser(userRepository);
+      createUserService.execute({
+        name,
+        nickname: 'Doe',
+        password: '123456',
+        email: 'jdoe@mail.com',
+        birthday: parseISO('1990-12-12')
+      }).then(data => {
+        console.log(data)
+      });
+      // throw new AppError('a');
+
+    }).toThrow(AppError);
+  })
+
+
 
 });
